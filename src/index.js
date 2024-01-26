741c17cf9e4334b0286d242ee25e71108eefbd5c
Ariuka <ariukats9@gmail.com>
2024-01-27 00:13:51 +0800

    asd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/test';
 import './index.css';
 import App from './App';  
 import 'react-toastify/dist/ReactToastify.css';
